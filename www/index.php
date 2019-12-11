0f71a125e2342f0771adbbfa647fe2e638d8173c
xiaoshangmin <778900778@qq.com>
2019-12-11 14:56:41 +0800

    fix

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<?php
+phpinfo();
+// 连接Redis
+//$redis = new Redis();
+//$redis->connect('redis', 6379);
+//链接数据库
+// try{
+//     $pdo = new PDO("mysql:host=mysql;dbname=mysql",'root','123456');
+//     print_r($pdo);
+// }catch(Exception $e){
+//     echo $e->getMessage();
+// }
