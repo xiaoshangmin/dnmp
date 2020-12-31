9700b783f6bf7a8bc133379ae400d57ec87547d9
xiaoshangmin <shangminxiao@gmail.com>
2021-01-01 01:00:35 +0800

    fix php

--- php7.4/ext/install.sh
+++ php7.4/ext/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-apk --update add --no-cache --virtual .build-deps autoconf g++ libtool \
-        make curl-dev gettext-dev linux-headers freetype \
-        freetype-dev libpng libpng-dev libjpeg-turbo libjpeg-turbo-dev libwebp-dev libzip-dev
+apk --update add --no-cache --virtual .build-deps autoconf g++ libtool make curl-dev linux-headers libevent-dev freetype-dev  libpng-dev libjpeg-turbo-dev
 
-docker-php-ext-configure gd --with-freetype --with-jpeg --with-webp \
+apk --no-cache add gettext-dev libzip-dev icu-dev freetype libpng libjpeg-turbo libwebp-dev rabbitmq-c-dev
+
+docker-php-ext-configure gd --with-freetype --with-jpeg --with-webp
 docker-php-ext-install -j$(nproc) gd
 docker-php-ext-install -j$(nproc) pcntl
 docker-php-ext-install -j$(nproc) pdo_mysql
@@ -17,11 +17,11 @@ docker-php-ext-install -j$(nproc) bcmath
 docker-php-ext-install -j$(nproc) opcache
 docker-php-ext-install -j$(nproc) zip
  
-# extName="redis"
-# mkdir ${extName}
-# tar -xf redis-5.2.0.tgz -C ${extName} --strip-components=1
-# (cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
-# docker-php-ext-enable ${extName}
+extName="redis"
+mkdir ${extName}
+tar -xf redis-5.2.0.tgz -C ${extName} --strip-components=1
+(cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
+docker-php-ext-enable ${extName}
 
 extName="swoole"
 mkdir ${extName}
@@ -29,10 +29,10 @@ tar -xf swoole-4.5.9.tgz -C ${extName} --strip-components=1
 (cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
 docker-php-ext-enable ${extName}
 
-# extName="amqp"
-# mkdir ${extName}
-# tar -xf amqp-1.10.2.tgz -C ${extName} --strip-components=1
-# (cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
-# docker-php-ext-enable ${extName}
+extName="amqp"
+mkdir ${extName}
+tar -xf amqp-1.10.2.tgz -C ${extName} --strip-components=1
+(cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
+docker-php-ext-enable ${extName}
 
 apk del .build-deps 
