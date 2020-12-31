39257c55cfd7bfda4d016ee520cc19c783fccdab
xiaoshangmin <Xsmxsm0220@gmail.com>
2020-12-31 17:40:36 +0800

    update

--- php7.4/ext/install.sh
+++ php7.4/ext/install.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
+apk --update add --no-cache --virtual .build-deps autoconf g++ libtool \
+        make curl-dev gettext-dev linux-headers freetype \
+        freetype-dev libpng libpng-dev libjpeg-turbo libjpeg-turbo-dev libwebp-dev libzip-dev
 
-extName="redis"
-mkdir ${extName}
-tar -xf redis-5.2.0.tgz -C ${extName} --strip-components=1
-(cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
-docker-php-ext-enable ${extName}
+docker-php-ext-configure gd --with-freetype --with-jpeg --with-webp \
+docker-php-ext-install -j$(nproc) gd
+docker-php-ext-install -j$(nproc) pcntl
+docker-php-ext-install -j$(nproc) pdo_mysql
+docker-php-ext-install -j$(nproc) mysqli
+docker-php-ext-install -j$(nproc) exif
+docker-php-ext-install -j$(nproc) sockets
+docker-php-ext-install -j$(nproc) gettext
+docker-php-ext-install -j$(nproc) intl
+docker-php-ext-install -j$(nproc) soap
+docker-php-ext-install -j$(nproc) bcmath
+docker-php-ext-install -j$(nproc) opcache
+docker-php-ext-install -j$(nproc) zip
+ 
+# extName="redis"
+# mkdir ${extName}
+# tar -xf redis-5.2.0.tgz -C ${extName} --strip-components=1
+# (cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
+# docker-php-ext-enable ${extName}
 
 extName="swoole"
 mkdir ${extName}
@@ -12,8 +29,10 @@ tar -xf swoole-4.5.9.tgz -C ${extName} --strip-components=1
 (cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
 docker-php-ext-enable ${extName}
 
-extName="amqp"
-mkdir ${extName}
-tar -xf amqp-1.10.2.tgz -C ${extName} --strip-components=1
-(cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
-docker-php-ext-enable ${extName}
+# extName="amqp"
+# mkdir ${extName}
+# tar -xf amqp-1.10.2.tgz -C ${extName} --strip-components=1
+# (cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
+# docker-php-ext-enable ${extName}
+
+apk del .build-deps 
