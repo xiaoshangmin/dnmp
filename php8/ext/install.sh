c3c90aa0f9be4cb1e508a31ce6e61556189ad1a2
xiaoshangmin <shangminxiao@gmail.com>
2021-01-10 19:51:26 +0800

    add php8

--- php8/ext/install.sh
+++ php8/ext/install.sh
@@ -19,19 +19,13 @@ docker-php-ext-install -j$(nproc) zip
  
 extName="redis"
 mkdir ${extName}
-tar -xf redis-5.2.0.tgz -C ${extName} --strip-components=1
+tar -xf redis-5.3.2.tgz -C ${extName} --strip-components=1
 (cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
 docker-php-ext-enable ${extName}
 
 extName="swoole"
 mkdir ${extName}
-tar -xf swoole-4.5.9.tgz -C ${extName} --strip-components=1
-(cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
-docker-php-ext-enable ${extName}
-
-extName="amqp"
-mkdir ${extName}
-tar -xf amqp-1.10.2.tgz -C ${extName} --strip-components=1
+tar -xf swoole-4.6.0.tgz -C ${extName} --strip-components=1
 (cd ${extName} && phpize && ./configure && make -j$(nproc) && make install)
 docker-php-ext-enable ${extName}
 
