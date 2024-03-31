7d8e3cd3e87ee9a1df054ede72d259a09686d5f7
xiaoshangmin <shangminxiao@gmail.com>
2024-04-01 00:49:22 +0800

    u

--- services/php8/ext/install.sh
+++ services/php8/ext/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+docker-php-ext-configure gd --with-freetype --with-jpeg --with-webp
+docker-php-ext-install -j$(nproc) gd
 
 docker-php-ext-install -j$(nproc) pcntl
 docker-php-ext-install -j$(nproc) pdo_mysql
@@ -11,7 +13,9 @@ docker-php-ext-install -j$(nproc) soap
 docker-php-ext-install -j$(nproc) bcmath
 docker-php-ext-install -j$(nproc) opcache
 docker-php-ext-install -j$(nproc) zip
- 
+
+pecl install imagick
+docker-php-ext-enable imagick
 pecl install redis
 docker-php-ext-enable redis
 pecl install -D 'enable-sockets="no" enable-openssl="yes" enable-http2="yes" enable-mysqlnd="yes" enable-swoole-json="no" enable-cares="yes"' swoole
