292c6f3cbe6378c7fa8c0c528297e57a73d6d65d
xiaoshangmin <shangminxiao@gmail.com>
2024-12-08 13:09:53 +0800

    u

--- services/php8.2/ext/install.sh
+++ services/php8.2/ext/install.sh
@@ -16,15 +16,12 @@ docker-php-ext-install -j$(nproc) opcache
 docker-php-ext-install -j$(nproc) zip
 
 pecl channel-update pecl.php.net
-#pecl install imagick
-#docker-php-ext-enable imagick
 pecl install phpy
 docker-php-ext-enable phpy
-# pecl install ffmpeg
-# docker-php-ext-enable ffmpeg
 pecl install redis
 docker-php-ext-enable redis
 pecl install -D 'enable-sockets="yes" enable-openssl="yes" enable-http2="yes" enable-mysqlnd="yes" enable-swoole-json="no"' swoole
 docker-php-ext-enable swoole
 docker-php-source delete
+rm -rf /tmp/pear
 
